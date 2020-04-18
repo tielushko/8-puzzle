c83a615c909df7cb211911333b984f63b4183e81
TurnTableTyler <51184665+TurnTableTyler@users.noreply.github.com>
2020-04-17 20:52:19 -0400

    Added the new stuff

--- driver.cpp
+++ driver.cpp
@@ -1,33 +1,10 @@
-#include <iostream>
+#include "PuzzleSolver.h"
+using namespace std;
 
-#include "state.h"
-
-//using namespace Algorithms;
-
-namespace Algorithms {
-    void DFS(State initial, State goal);
-    void BFS(State initial, State goal);
-    void Dijkstra(State initial, State goal);
-}
-
-int main() {
-    Graph graph;    
-    StateArray array = {1,2,4,0,3,5,6,7,8};
-    State state(ROW_COL_COUNT, array);
-
-    std::shared_ptr<Node> node;
-    Node()
-    /*
-    for (int i = 0; i < test.size(); i++) {
-        if(i % 3 == 0) 
-            std::cout << "\n";
-            
-        std::cout << test.at(i) << " ";
-    }*/
-
-    //using namespace Algorithms;
-
-    //std::cout << Algorithms::a;
-
-    return 0;
+int main(){
+   Graph graph;
+   step step;
+   step.board = {0,1,2,3,4,5,6,7,8};
+   graph.DFS(step.board);
+   return 0;
 }
